e71021a97a0f828a955f3ed732088c3e873ffb6c
Thiago dos Santos Grillo <thiago10gr@gmail.com>
2017-03-26 22:37:09 -0300

    remoção de pacotes não utilizados

--- PadraoWS/src/main/java/br/com/thiago10gr/resources/UsuarioResource.java
+++ PadraoWS/src/main/java/br/com/thiago10gr/resources/UsuarioResource.java
@@ -4,13 +4,14 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import br.com.thiago10gr.model.Usuario;
 import br.com.thiago10gr.service.ServiceException;
 import br.com.thiago10gr.service.UsuarioService;
@@ -18,8 +19,6 @@ import br.com.thiago10gr.util.CalendarDateJsonSerializer;
 import br.com.thiago10gr.util.CalendarDateTimeJsonSerializer;
 import br.com.thiago10gr.util.DateTime;
 import br.com.thiago10gr.util.Secured;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 @Path("usuario")
 public class UsuarioResource {
